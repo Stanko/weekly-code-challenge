7c25e53543ad5c2a87a313fb1caccc6986d3d2df
Stanko <stanko.tadic@gmail.com>
2021-05-24 12:25:05 +0200

    Week eight

---
+++ src/week-eight/js/index.js
@@ -0,0 +1,7 @@
+// const prism = document.querySelector('.prism');
+
+// window.addEventListener('mousemove', (e) => {
+//   prism.style.transform = `rotateY(${
+//     (e.clientX / window.innerWidth) * 360
+//   }deg) rotateX(0deg)`;
+// });
